acc4231dd311888182261f21376c630bb3cbc9bd
huangqingtian <huangqingtian@gzhtedu.cn>
2021-03-12 11:27:57 +0800

    涉及模块： 修改内容：修复启动报错、删除hp模块 提交人：

---
+++ app/src/main/java/com/example/mainandroid/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mainandroid
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(View(this))
+    }
+}
